302ca64eaec8f7518cbe613c1d0062d472e2134a
KoiHooks <79552205+KoiHooks@users.noreply.github.com>
2022-02-08 06:12:56 -0800

    Add files via upload

---
+++ RegisterBetterWorkLikeHoly/RegisterBetterWorkLikeHoly/Login.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace RegisterBetterWorkLikeHoly
+{
+    partial class Log
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.key = new System.Windows.Forms.TextBox();
+            this.Lgn = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Rgstr = new System.Windows.Forms.Button();
+            this.passlabel = new System.Windows.Forms.Label();
+            this.userlabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(21)))), ((int)(((byte)(36)))));
+            this.username.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.username.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.username.Location = new System.Drawing.Point(15, 78);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(361, 24);
+            this.username.TabIndex = 0;
+            // 
+            // password
+            // 
+            this.password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(21)))), ((int)(((byte)(36)))));
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.password.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.password.Location = new System.Drawing.Point(15, 125);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(361, 24);
+            this.password.TabIndex = 1;
+            // 
+            // key
+            // 
+            this.key.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(21)))), ((int)(((byte)(36)))));
+            this.key.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.key.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.key.Location = new System.Drawing.Point(12, 172);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(361, 24);
+            this.key.TabIndex = 3;
+            // 
+            // Lgn
+            // 
+            this.Lgn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(21)))), ((int)(((byte)(36)))));
+            this.Lgn.FlatAppearance.BorderSize = 0;
+            this.Lgn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Lgn.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.Lgn.ForeColor = System.Drawing.Color.White;
+            this.Lgn.Location = new System.Drawing.Point(12, 225);
+            this.Lgn.Name = "Lgn";
+            this.Lgn.Size = new System.Drawing.Size(111, 34);
+            this.Lgn.TabIndex = 4;
+            this.Lgn.Text = "Login";
+            this.Lgn.UseVisualStyleBackColor = false;
+            this.Lgn.Click += new System.EventHandler(this.Lgn_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(21)))), ((int)(((byte)(36)))));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(362, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 25);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Login";
+            this.label1.Visible = false;
+            // 
+            // Rgstr
+            // 
+            this.Rgstr.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(21)))), ((int)(((byte)(36)))));
+            this.Rgstr.FlatAppearance.BorderSize = 0;
+            this.Rgstr.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Rgstr.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.Rgstr.ForeColor = System.Drawing.Color.White;
+            this.Rgstr.Location = new System.Drawing.Point(265, 225);
+            this.Rgstr.Name = "Rgstr";
+            this.Rgstr.Size = new System.Drawing.Size(111, 34);
+            this.Rgstr.TabIndex = 11;
+            this.Rgstr.Text = "Register";
+            this.Rgstr.UseVisualStyleBackColor = false;
+            this.Rgstr.Click += new System.EventHandler(this.Rgstr_Click);
+            // 
+            // passlabel
+            // 
+            this.passlabel.AutoSize = true;
+            this.passlabel.BackColor = System.Drawing.Color.Transparent;
+            this.passlabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passlabel.ForeColor = System.Drawing.Color.White;
+            this.passlabel.Location = new System.Drawing.Point(12, 105);
+            this.passlabel.Name = "passlabel";
+            this.passlabel.Size = new System.Drawing.Size(66, 17);
+            this.passlabel.TabIndex = 14;
+            this.passlabel.Text = "Password";
+            // 
+            // userlabel
+            // 
+            this.userlabel.AutoSize = true;
+            this.userlabel.BackColor = System.Drawing.Color.Transparent;
+            this.userlabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userlabel.ForeColor = System.Drawing.Color.White;
+            this.userlabel.Location = new System.Drawing.Point(12, 58);
+            this.userlabel.Name = "userlabel";
+            this.userlabel.Size = new System.Drawing.Size(69, 17);
+            this.userlabel.TabIndex = 13;
+            this.userlabel.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(15, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Key";
+            // 
+            // Log
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(16)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(385, 271);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.passlabel);
+            this.Controls.Add(this.userlabel);
+            this.Controls.Add(this.Rgstr);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Lgn);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Log";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.Button Lgn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Rgstr;
+        private System.Windows.Forms.Label passlabel;
+        private System.Windows.Forms.Label userlabel;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RegisterBetterWorkLikeHoly/RegisterBetterWorkLikeHoly/Login.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KeyAuth;
+namespace RegisterBetterWorkLikeHoly
+{
+    public partial class Log : Form
+    {
+
+        static string name = "Xerox";
+        static string ownerid = "2zD2jmxrGy";
+        static string secret = "d5c764bcfa825e1fa57e587d28cf5edc35f367ad0b2a01b1332968fac13a35ef";
+        static string version = "1.0";
+        public static api KeyAuthApp = new api(name, ownerid, secret, version);
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        public Log()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            KeyAuthApp.init();
+        }
+
+        private void Lgn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rgstr_Click(object sender, EventArgs e)
+        {
+            KeyAuthApp.register(username.Text, password.Text, key.Text);
+            if (KeyAuthApp.response.success)
+            {
+                MessageBox.Show("Registered");
+            }
+        }
+    }
+}
